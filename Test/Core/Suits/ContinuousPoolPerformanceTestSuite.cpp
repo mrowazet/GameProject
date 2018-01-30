115159a236a456989966faec2456ee5132f30897
Michał Cłapa <mrowawow@live.com>
2018-01-30 21:11:53 +0100

    first impl of smart ptr test

--- Test/Core/Suits/ContinuousPoolPerformanceTestSuite.cpp
+++ Test/Core/Suits/ContinuousPoolPerformanceTestSuite.cpp
@@ -13,6 +13,7 @@ namespace
 {
 	const bool ENABLED = true;
 	const bool DISABLED = false;
+	const bool COMPARE_WITH_SMART_PTRS = true;
 
 	const PoolSize POOL_SIZE = 10000u;
 	const u32 LOOPS = 100u;
@@ -67,6 +68,47 @@ public:
 		stopStopwatch();
 	}
 
+	template<typename T>
+	std::vector<T> createVector()
+	{
+		std::vector<T> l_vector;
+		l_vector.resize(POOL_SIZE);
+
+		return l_vector;
+	}
+
+	template<typename T>
+	void repeatActionForNumberOfLoops(std::vector<T>& p_vector, std::function<void(std::vector<T>&)> p_function)
+	{
+		for (auto i = 0u; i < LOOPS; i++)
+		{
+			p_function(p_vector);
+		}
+	}
+
+	template<typename T>
+	void measureActionOnCreatedVector(std::function<void(std::vector<T>&)> p_function)
+	{
+		std::cout << "Size of tested entity: " << sizeof(T) << "\n";
+		auto l_vector = createVector<T>();
+
+		startStopwatch();
+		repeatActionForNumberOfLoops(l_vector, p_function);
+		stopStopwatch();
+	}
+	
+	template<typename T>
+	auto allocateAllElementsInVector()
+	{
+		auto l_function = [](auto& p_vector)
+		{
+			for (int i = 0u; i < POOL_SIZE; i++)
+				p_vector[0].reset(new T());
+		};
+		
+		return l_function;
+	}
+
 protected:
 	testTool::Stopwatch m_stopwatch;
 };
@@ -79,7 +121,7 @@ TEST_F(ContinuousPoolPerformanceTestSuite, allocateWholePoolAndClear)
 	auto l_allocateWholePoolAndThenClear = [](auto& p_pool)
 	{
 		for (int i = 0u; i < POOL_SIZE; i++)
-			auto& l_element = p_pool.allocate();
+			p_pool.allocate();
 
 		p_pool.clear();
 	};
@@ -91,6 +133,12 @@ TEST_F(ContinuousPoolPerformanceTestSuite, allocateWholePoolAndClear)
 	measureActionOnCreatedPool<Entity128>(l_allocateWholePoolAndThenClear);
 	measureActionOnCreatedPool<Entity256>(l_allocateWholePoolAndThenClear);
 	measureActionOnCreatedPool<Entity512>(l_allocateWholePoolAndThenClear);
+
+	if (!COMPARE_WITH_SMART_PTRS)
+		return;
+
+	measureActionOnCreatedVector<std::shared_ptr<Entity8>>(allocateAllElementsInVector<Entity8>());
+	measureActionOnCreatedVector<std::unique_ptr<Entity8>>(allocateAllElementsInVector<Entity8>());
 }
 
 TEST_F(ContinuousPoolPerformanceTestSuite, allocateWholePoolAndReset)
